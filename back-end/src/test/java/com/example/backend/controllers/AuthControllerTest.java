c02f2e633d5bccb360c12ff63e242576a7d107f2
Asma Mestaysser <110032159+asmamest@users.noreply.github.com>
2025-10-22 14:33:22 +0100

    Add AutoConfigureMockMvc to AuthControllerTest

--- back-end/src/test/java/com/example/backend/controllers/AuthControllerTest.java
+++ back-end/src/test/java/com/example/backend/controllers/AuthControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.backend.controllers;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.http.MediaType;
