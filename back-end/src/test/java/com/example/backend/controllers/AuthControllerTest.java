e83ae00db70d6847a244ac20715caf2a26598c87
Asma Mestaysser <110032159+asmamest@users.noreply.github.com>
2025-10-22 16:03:03 +0100

    Add ActiveProfiles annotation to AuthControllerTest

--- back-end/src/test/java/com/example/backend/controllers/AuthControllerTest.java
+++ back-end/src/test/java/com/example/backend/controllers/AuthControllerTest.java
@@ -20,6 +20,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@ActiveProfiles("test")
 public class AuthControllerTest {
 
     @LocalServerPort
