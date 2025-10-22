56381b029085739a9b3f7854087c7fc1b43cb888
Asma Mestaysser <110032159+asmamest@users.noreply.github.com>
2025-10-22 14:13:10 +0100

    Change test configuration to use WebMvcTest

--- back-end/src/test/java/com/example/backend/controllers/AuthControllerTest.java
+++ back-end/src/test/java/com/example/backend/controllers/AuthControllerTest.java
@@ -18,8 +18,7 @@ import org.springframework.test.annotation.DirtiesContext;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@WebMvcTest(AuthController.class)
 public class AuthControllerTest {
 
     @LocalServerPort
